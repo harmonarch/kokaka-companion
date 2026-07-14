e40fa52dfdcb4d8764c952c1fa2f4f3634d31d06
thomasquan605 <thomas.quan605@gmail.com>
2026-07-14 22:32:06 +0800

    Add Sentry build dependencies

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,10 +1,12 @@
 const path = require("path")
-const { getDefaultConfig } = require("expo/metro-config")
+const { getSentryExpoConfig } = require("@sentry/react-native/metro")
 
 const projectRoot = __dirname
 const workspaceRoot = path.resolve(projectRoot, "../..")
 
-const config = getDefaultConfig(projectRoot)
+const config = getSentryExpoConfig(projectRoot, {
+  includeWebReplay: false,
+})
 
 config.watchFolders = [workspaceRoot]
 config.resolver.unstable_enableSymlinks = true
